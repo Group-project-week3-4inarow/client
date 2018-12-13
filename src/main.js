124884772acd74ea3520009566c0647cf77dfba6
sendyakbar <msendyakbars@gmail.com>
2018-12-13 20:10:42 +0700

    fixing firebase error

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import firebase from "firebase"
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -16,13 +15,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-
-var config = {
-  apiKey: "AIzaSyAIVKQ6gKo8qwlOsZQ8tHOZ4razngr-mUc",
-  authDomain: "in-a-row-742b3.firebaseapp.com",
-  databaseURL: "https://in-a-row-742b3.firebaseio.com",
-  projectId: "in-a-row-742b3",
-  storageBucket: "in-a-row-742b3.appspot.com",
-  messagingSenderId: "245310019524"
-};
-firebase.initializeApp(config)
