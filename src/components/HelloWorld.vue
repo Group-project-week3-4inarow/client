06848be6d19be9ca70c91a1dc4fd16a2b49d1c12
Ribka Haryuni <ribka.blue@gmail.com>
2018-12-13 20:25:28 +0700

    testing on making rooms

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form>
+      Room Name:<br>
+      <input type="text" name="roomName" v-model="roomName"><br>
+      <input type="button" @click="writeUserData" value="SUBMIT!">
+    </form>
+    <div class="col-sm-4" >
+      <table style="width:100%" v-for="room in rooms" :key="room._id">
+        <tr>
+          <th>{{ room.name }}</th>
+        </tr>
+        <tr>
+          <td><router-link to="/about">JOIN</router-link></td>
+        </tr> 
+      </table>
+    </div> 
   </div>
 </template>
 
 <script>
+import db from '@/assets/firebase'
+
 export default {
   name: 'HelloWorld',
-  props: {
+  data() {
+    return {
+      roomName: "",
+      rooms: ""
+    }
+  },
+  props: {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
     msg: String
-  }
+  },                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+  methods: {
+    writeUserData() {
+      db.ref('rooms/' + this.roomName)
+      .set({
+        name: this.roomName,
+      });
+    },
+    readDB() {
+      db.ref('rooms/').on('value', (snapshot) => {
+        this.rooms = snapshot.val()
+      })
+    }
+  },
+  created() {
+    this.readDB()
+  }  
 }
-</script>
+
+</script>                                                                                                                                                                                                                                                                                             
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+h3 {                                                                                                                                                                                                                                                                                                    
   margin: 40px 0 0;
 }
 ul {
@@ -54,5 +67,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+table, th, td {
+  border: 2px solid black;
 }
 </style>
